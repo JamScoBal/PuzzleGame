2f17315f8726c7742bfd26c1df523a370c7021f6
Unknown <jameson.ballard21@gmail.com>
2016-11-24 22:02:12 -0700

    11/24/2016

---
+++ Assets/Scripts/lightSwitch.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightSwitch : MonoBehaviour {
+
+    public bool isOn = false;
+	
+	public void change()
+    {
+        if(isOn)
+        {
+            isOn = false;
+            this.transform.localEulerAngles = new Vector3(0, 45, 0);
+        }
+        else
+        {
+            isOn = true;
+            this.transform.localEulerAngles = Vector3.zero;
+        }
+    }
+}
