c9a55240c9bd3711fd37d8020dde6446f819bfcc
Unknown <jameson.ballard21@gmail.com>
2016-12-10 13:44:20 -0700

    28

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    public Canvas StartMenu;
+    public Button StartButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartMenu = StartMenu.GetComponent<Canvas>();
+        StartButton = StartButton.GetComponent<Button>();
+    }
+
+    public void StartPress()
+    {
+        StartButton.enabled = false;
+        StartMenu.enabled = false;
+    }
+}
